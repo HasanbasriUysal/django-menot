d2994ad2aa6598c0f55cb0913e3e36881f4c91ea
HasanbasriUysal <48217@edu.raseko.fi>
2023-11-21 13:04:31 +0200

    added

---
+++ moneyflow/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from .models import Account, Category, Document, Transaction
+
+
+@admin.register(Document)
+class DocumentAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Account)
+class AccountAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    list_display = [
+        "id",
+        "account",
+        "date",
+        "amount",
+        "type",
+        "state",
+        "category",
+        "comment",
+    ]
+    list_display_links = list_display
