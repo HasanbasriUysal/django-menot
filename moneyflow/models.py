28412754fe5189fdb6c2030492ef2db7394d57ef
HasanbasriUysal <48217@edu.raseko.fi>
2023-11-21 15:06:53 +0200

    verbose_name

--- moneyflow/models.py
+++ moneyflow/models.py
@@ -22,19 +22,23 @@ class OwnedModel(models.Model):
 
 class Document(TimestampModel, OwnedModel):
     class Type(models.TextChoices):
-        #KOODI_NIMI = ("TIETOKANTAAN TALLENNETTAVA", _("Kayttajalle nakyva"))
+        # KOODI_NIMI = ("TIETOKANTAAN TALLENNETTAVA", _("Kayttajalle nakyva"))
         BILL = ("BILL", _("Lasku"))
         RECEIPT = ("RECEIPT", _("Kuitti"))
         CALCULATION = ("CALCULATION", _("Laskelma"))
         OTHER = ("OTHER", _("Muu"))
 
     type = models.CharField(max_length=20, choices=Type.choices)
-    name = models.CharField(max_length=100,blank=True)
+    name = models.CharField(max_length=100, blank=True)
     file = models.FileField(upload_to="docs/%Y-%M/")
+
+    class Meta:
+        verbose_name = _("dokumentti")
+        verbose_name_plural = _("dokumentit")
 
     def __str__(self):
         return self.name if self.name else f"Document{self.id}"
-    
+
 
 class Category(TimestampModel, OwnedModel):
     name = models.CharField(max_length=100)
@@ -44,31 +48,41 @@ class Category(TimestampModel, OwnedModel):
         null=True,
         related_name="subcategorie",
         on_delete=models.CASCADE,
-    )   
+    )
+
+    class Meta:
+        verbose_name = _("kategoria")
+        verbose_name_plural = _("kategoriat")
 
     def __str__(self):
-       prefix = f"{self.parent} / " if self.parent else ""
-       return f"{prefix}{self.name}"
+        prefix = f"{self.parent} / " if self.parent else ""
+        return f"{prefix}{self.name}"
 
 
-class Account(TimestampModel, OwnedModel):    
+class Account(TimestampModel, OwnedModel):
     name = models.CharField(max_length=100)
     bank_account = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("tili")
+        verbose_name_plural = _("tilit")
 
     def __str__(self):
         return f"{self.id:04d} {self.name}"
 
 
 class Transaction(TimestampModel):
-    class Type(models.TextChoices):     
+    class Type(models.TextChoices):
         INCOME = ("INCOME", _("Tulo"))
         EXPENSE = ("EXPENSE", _("Meno"))
-    
+
     class State(models.TextChoices):
         UPCOMING = ("UPCOMING", _("Tuleva"))
         DONE = ("DONE", _("Tapahtunut"))
 
-    account = models.ForeignKey(Account, on_delete=models.RESTRICT)
+    account = models.ForeignKey(
+        Account, on_delete=models.RESTRICT, verbose_name=_("tili")
+    )
     type = models.CharField(max_length=20, choices=Type.choices)
     state = models.CharField(max_length=20, choices=State.choices)
     date = models.DateField()
@@ -82,6 +96,9 @@ class Transaction(TimestampModel):
         blank=True,
     )
 
+    class Meta:
+        verbose_name = _("tilitapahtuma")
+        verbose_name_plural = _("tilitapahtumat")
+
     def __str__(self):
         return f"{self.date} {self.account} {self.amount} ({self.state})"
-    
