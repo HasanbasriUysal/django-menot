6225e58dfc068536b47f092c3cdac6a082bc0ee5
HasanbasriUysal <48217@edu.raseko.fi>
2023-11-21 14:21:50 +0200

    added model admine

--- moneyflow/admin.py
+++ moneyflow/admin.py
@@ -4,7 +4,25 @@ from django.contrib.admin import ModelAdmin
 from .models import Account, Category, Document, Transaction
 
 
+@admin.register(Document)
+class DocumentAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    pass
+
+
 @admin.register(Account)
 class AccountAdmin(ModelAdmin):
     pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    pass
+
+
+
 
