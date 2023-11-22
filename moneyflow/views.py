8d8be6097b3be06ea18a544ae1bbc29f9be0a01a
HasanbasriUysal <48217@edu.raseko.fi>
2023-11-22 12:43:09 +0200

    nayta tilit etusivulla

--- moneyflow/views.py
+++ moneyflow/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
+from .models import Account
 
-def frontpage(request):
-    return render(request, "moneyflow/index.html")
+def frontpage(request):    
+    if request.user.is_anonymous:
+        return redirect(reverse("admin:login"), kwargs={"next":"/"})
+    context = {
+        "accounts": Account.objects.filter(owner=request.user),
+    }
+    return render(request, "moneyflow/index.html", context)
